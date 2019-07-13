de6fe461b4b576fb10713f78d0f1b3463c38852a
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-07-13 10:13:43 +0100

    create meetup-store.js

---
+++ src/Meetups/meetups-store.js
@@ -0,0 +1,56 @@
+import { writable } from 'svelte/store'
+
+const meetups = writable([
+    {
+        id: "m1",
+        title: "Coding Bootcamp",
+        subtitle: "Learn to code in 2 hours",
+        description:
+            "In this meetup, we will have some experts that teach you how to code.",
+        imageUrl:
+            "https://www.publicdomainpictures.net/pictures/150000/velka/coffee-break-1454539196eJw.jpg",
+        address: "27 Nerd Road, 34578 New York",
+        contact: "code@test.com",
+        isFavorite: false
+    },
+    {
+        id: "m2",
+        title: "Swim Together",
+        subtitle: "Learn to code in 2 hours",
+        description: "We will simply swim some rounds aboot the place.",
+        imageUrl:
+            "https://www.publicdomainpictures.net/pictures/10000/velka/man-swimming-in-pool-871278587620eIzC.jpg",
+        address: "7 Nerd Street, 34578 Jersey",
+        contact: "swim@test.com",
+        isFavorite: false
+    }
+])
+
+const customMeetupsStore = {
+    subscribe: meetups.subscribe,
+    addMeetup: meetupData => {
+        const newMeetup = {
+            ...meetupData,
+            id: Math.random().toString(),
+            isFavorite: false
+        }
+        meetups.update(items => {
+            return [
+                ...items,
+                newMeetup
+            ]
+        })
+    },
+    toggleFavorite: id => {
+        meetups.update(items => {
+            const updatedMeetup = { ...items.find(m => m.id === id) };
+            updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+            const meetupIndex = items.findIndex(m => m.id === id);
+            const updatedMeetups = [...items];
+            updatedMeetups[meetupIndex] = updatedMeetup;
+            return updatedMeetups;
+        })
+    }
+}
+
+export default customMeetupsStore
